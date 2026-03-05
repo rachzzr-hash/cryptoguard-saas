1fed42ff81537baf738b50235e3ffa5437f73591
rachzzr-hash <rachzzr@gmail.com>
2026-03-05 08:50:33 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./client/src/**/*.{js,ts,jsx,tsx}", "./client/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
